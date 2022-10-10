3b9b980f983366280bef9770415a6478c6024570
EquableJarl <alexjohngregory@gmail.com>
2022-10-10 21:19:29 +0100

    adding untested check choice

--- game.py
+++ game.py
@@ -8,13 +8,23 @@ with open(json_file_path, "r") as j:
     data = json.loads(j.read())
 
 
+def check_choice(chap):
+    choice = input("what do you want to do? : ")
+    if choice in data[chap]["options"]:
+        return choice
+    else : 
+        print("That was not a valid choice ... ")
+        return
+
+
 def print_text(chap, d):
     global chapter
     print(d[chap]["text"])
     for i in data[chap]["options"]:
         print(i + ") " + data[chap]["options"][i]["text"]) 
-    choice = input("what do you want to do? : ")
-    chapter = data[chap]["options"][choice]["direction"]
+    choice = check_choice(chap)
+    if choice != None:
+        chapter = data[chap]["options"][choice]["direction"]
 
 while is_alive == True :
     print_text(chapter, data)
